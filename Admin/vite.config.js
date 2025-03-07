b3efbc3249c758ae5754a4cda4869610c4321474
sankar <sankarjyotichetia@gmail.com>
2025-03-07 16:47:00 +0530

    ,,

--- Admin/vite.config.js
+++ Admin/vite.config.js
@@ -3,5 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://systemicaltruim.onrender.com",
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
